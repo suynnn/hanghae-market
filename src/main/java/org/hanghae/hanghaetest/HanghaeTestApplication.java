0e67f0e0659131cb5e9fc5a00f29c35d3b708710
suynnn <suy00065@gmail.com>
2024-09-09 10:08:28 +0900

    Chore: 깃 초기설정

---
+++ src/main/java/org/hanghae/hanghaetest/HanghaeTestApplication.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hanghae/hanghaetest/HanghaeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
